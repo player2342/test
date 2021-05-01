c459ba6fbe3aec53dbe952d283ddb1a0bf1a2d1f
player2342 <71567726+player2342@users.noreply.github.com>
2021-05-01 13:45:52 -0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = 'World' } = request.query
-  response.status(200).send(`Hello ${name}!`)
+module.exports = (req, res) => {
+  const { name = 'World' } = req.query
+  res.status(200).send(`Hello ${name}!`)
 }
